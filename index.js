6d5f63cff72859a904353c8c692e788506993872
LocatedReaper <82606980+LocatedBiome@users.noreply.github.com>
2021-04-23 13:20:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-var counter=0;
-function moveBy10(x){
-  var width =10;
-  var bar = document.getElementById('bar');
-  counter++;
-  if(counter*x < 101){
-    bar.style.width = counter*x +'%';
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 10;
+    var id = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+        elem.innerHTML = width + "%";
+      }
+    }
   }
 }
